15e3cae46d2fc2ce81574a8bc711a79902b6d037
David <gkim51@students.kennesaw.edu>
2025-05-19 01:44:28 -0400

    added thmyeleaf & tailwind into project

---
+++ src/main/java/com/geonhyeongkim/portfolio/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.geonhyeongkim.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("name", "Alice");
+		return "home";
+	}
+
+}
