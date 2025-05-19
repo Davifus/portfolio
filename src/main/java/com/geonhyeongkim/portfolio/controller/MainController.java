3f95e8308b3195d7a6bb72fc74769ef96205b3aa
David <gkim51@students.kennesaw.edu>
2025-05-19 02:23:17 -0400

    add more to home template

--- src/main/java/com/geonhyeongkim/portfolio/controller/MainController.java
+++ src/main/java/com/geonhyeongkim/portfolio/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("name", "Alice");
+		model.addAttribute("name", "Geonhyeong Kim");
 		return "home";
 	}
 
